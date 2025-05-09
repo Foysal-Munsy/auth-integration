b8306b982c5917fcf6ee0876c96a0dea7c941f71
Foysal-Munsy <bdfoysal555@gmail.com>
2025-05-09 06:29:04 +0600

    login authContext

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../firebase.init";
 
 const AuthProvider = ({ children }) => {
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
   const userInfo = {
-    // email: "pot@alu.com",
     createUser,
+    signInUser,
   };
   return <AuthContext value={userInfo}>{children} </AuthContext>;
 };
